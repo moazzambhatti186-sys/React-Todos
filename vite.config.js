4e773b2aa500fc9de7579a75abb08aa42852b30f
Moazzam <your-email@example.com>
2026-06-17 01:06:03 +0500

    istall bootstrap sass react-router-dom

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Node built-in path module
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@/': path.resolve(__dirname, './src'), // Maps '@' to the 'src' directory
+    },
+  },
 })
