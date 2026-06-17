4f8247cac8eac0316f2a7c0d5a5fa3c59debf51d
Moazzam <your-email@example.com>
2026-06-17 20:14:51 +0500

    useAuthContext used

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createRoot } from 'react-dom/client'
 import "bootstrap/dist/js/bootstrap.bundle.js";
 
 import App from './App.jsx'
+
 import AuthContext from './context/Auth.jsx';
 
 createRoot(document.getElementById('root')).render(
--- src/pages/Dashboard/User.jsx
+++ src/pages/Dashboard/User.jsx
@@ -1,28 +1,49 @@
-// import About from "../Frontend/About/about"
-// import Contact from "../Frontend/Contact/contact"
-// import Hero from "../Frontend/Hero/hero"
-// import Home from "../Frontend/Home/home"
+import { Col, Divider, Row, Typography } from "antd"
+import { useAuthContext } from "../../context/Auth"
 
-import { Col, Row, Typography } from "antd"
-
-const {Title} = Typography
+const { Title } = Typography
 
 const User = () => {
-    return (
-        <>
-            {/* <Hero />
-            <Home />
-            <About />
-            <Contact /> */}
 
-<div className="container">
-    <Row>
-        <Col span={24} className="text-center">
-        <Title level={1}>User Dashboard</Title>
-        </Col>
-    </Row>
-</div>
-        </>
+    const { user, setUser } = useAuthContext()
+
+    return (
+        <main className="bg-primary">
+            <div className="container mt-5">
+                <Row>
+                    <Col span={24} className="text-center py-4">
+                        <Title level={1} className="text-light">User Dashboard</Title>
+                        <hr style={{ border: '2px solid white' }} />
+                    </Col>
+                    <Col span={24}>
+                        <table class="table text-center">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Full Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Password</th>
+                                    <th scope="col">Confirm Password</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">UID</th>
+                                </tr>
+                            </thead>
+                            {
+                                <tbody>
+                                    <tr>
+                                        <td> {user.fullName} </td>
+                                        <td> {user.email} </td>
+                                        <td> {user.password} </td>
+                                        <td> {user.confirmPassword} </td>
+                                        <td> {user.status} </td>
+                                        <td> {user.id} </td>
+                                    </tr>
+                                </tbody>
+                            }
+                        </table>
+                    </Col>
+                </Row>
+            </div>
+        </main>
 
     )
 }
